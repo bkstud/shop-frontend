fc549081fc8a5f7bcf8f289d533b7a1db74431da
Bazyli Kot <ciobab97@gmail.com>
2022-07-09 15:55:16 +0200

    initial header with working login buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import './components/LoginBox'
 import LoginBox from './components/LoginBox';
+import TopHeader from './components/Header';
+
 function App() {
   return (
     <div className="App">
+      <TopHeader/>
       <LoginBox/>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import GithubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
+
+const BACKEND_URL = "https://localhost:5000/api/v1"
+
+export default function TopHeader(props) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Shop
+          </Typography>
+          <Button color="inherit" disabled={true}>Login via:</Button>
+          <a href={BACKEND_URL + "/login/github"}>
+              <IconButton color="inherit" aria-label="github login" href="url"
+                          component="span">
+                <GithubIcon/>
+              </IconButton>  
+            </a>
+          <a href={BACKEND_URL + "/login/facebook"}>
+            <IconButton color="inherit" aria-label="facebook login" component="span">
+              <FacebookIcon />
+            </IconButton>
+          </a>
+          
+          <a href={BACKEND_URL + "/login/google"}>
+            <IconButton color="inherit" aria-label="google login" component="span">
+              <GoogleIcon/>
+            </IconButton>
+          </a>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
