cdd876e535c42cb452f78d88729f4809afc4333a
Bazyli Kot <ciobab97@gmail.com>
2022-07-14 01:11:09 +0200

    working on listing items

---
+++ src/components/ItemGrid.jsx
@@ -0,0 +1,26 @@
+
+import * as React from 'react'; 
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Item from './Item' 
+
+export default function ItemGrid(props) {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={{ xs: 2, md: 1 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+           { 
+           props.items.forEach( (it) =>
+                {
+                <Grid item xs={4}>
+                    <Item>xs=8</Item>
+               </Grid>}
+            )
+           }
+           
+           <Grid item xs={4}>
+              <Item>xs=8</Item>
+           </Grid>
+          </Grid>
+        </Box>
+      );
+    }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,15 +1,19 @@
 import Header from './Header';
+import ItemGrid from './ItemGrid' 
 import useUser from '../hooks/User'
 import useItems from '../hooks/Items'
 import useBasket from '../hooks/Basket'
+
 
 export default function Page(props) {
   const {user, setUser} = useUser()
   const {items, setItems} = useItems()
   const {basket, setBasket} = useBasket()
+
   return (
       <div>
         <Header user={user} setUser={setUser}/>
+        <ItemGrid items={items} basket={{basket, setBasket}}/>
       </div>
     )
 }
