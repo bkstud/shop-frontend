4c43d221f88ecfa35464637c95b0ee7d49dde8c9
Bazyli Kot <ciobab97@gmail.com>
2022-08-04 16:02:51 +0200

    fix rendering when there is no user

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -14,62 +14,64 @@ import Typography from '@mui/material/Typography';
 export default function Orders(props) {
     const orders = props.transactions;
     const user = props.user;
-    return (
-        <div>
+    if(!user) {
+        return (<Alert variant="outlined" severity="info">
+        Please login to see your orders.
+    </Alert>)
+    } else {
+        return (
+            
+            <Grid container spacing={3}>
+            <Grid item xs={12} xm={12}></Grid>
+            <Grid item>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 150, maxWidth: 750 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                        <TableCell align="center">
+                            <Typography variant="h6" component="div">
+                                Your orders:
+                            </Typography>
+                        </TableCell><TableCell/><TableCell/><TableCell/><TableCell/>
+                        </TableRow>
+                        <TableRow>
+                        <TableCell>Product</TableCell>
+                        <TableCell align="left">Price</TableCell>
+                        <TableCell align="left">Type</TableCell>
+                        <TableCell align="left">Payment status</TableCell>
+                        <TableCell align="left">Realization status</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {orders.map((order) => (
+                        <TableRow
+                            key={order.ID}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">{order.Item.Name}</TableCell>
+                            <TableCell align="left">{order.Item.Price}$</TableCell>
+                            <TableCell align="left">{order.Type}</TableCell>
+                            <TableCell align="center">{order.Payment}</TableCell>
+                            <TableCell align="center">{order.Status}</TableCell>
+                        </TableRow>
+                        ))}
+                        {
+                        (!user) && (
+                        <TableRow>
+                            <TableCell/><TableCell/>
+                                <TableCell align="left">
+
+                            </TableCell>
+                        </TableRow>)
+                        }
         
-        <Grid container spacing={3}>
-        <Grid item xs={12} xm={12}></Grid>
-        <Grid item>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 150, maxWidth: 750 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                    <TableCell align="center">
-                        <Typography variant="h6" component="div">
-                            Your orders:
-                        </Typography>
-                    </TableCell><TableCell/><TableCell/><TableCell/><TableCell/>
-                    </TableRow>
-                    <TableRow>
-                    <TableCell>Product</TableCell>
-                    <TableCell align="left">Price</TableCell>
-                    <TableCell align="left">Type</TableCell>
-                    <TableCell align="left">Payment status</TableCell>
-                    <TableCell align="left">Realization status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {orders.map((order) => (
-                    <TableRow
-                        key={order.ID}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                        <TableCell component="th" scope="row">{order.Item.Name}</TableCell>
-                        <TableCell align="left">{order.Item.Price}$</TableCell>
-                        <TableCell align="left">{order.Type}</TableCell>
-                        <TableCell align="center">{order.Payment}</TableCell>
-                        <TableCell align="center">{order.Status}</TableCell>
-                    </TableRow>
-                    ))}
-                    {
-                    (!user) && (
-                    <TableRow>
-                        <TableCell/><TableCell/>
-                            <TableCell align="left">
-                            <Alert variant="outlined" severity="info">
-                                Login to go to see your orders.
-                            </Alert>
-                        </TableCell>
-                    </TableRow>)
-                    }
-    
-                </TableBody>
-                </Table>
-    
-            </TableContainer>
-    
-        </Grid>
-        </Grid>
-        </div>
-        );
+                    </TableBody>
+                    </Table>
+        
+                </TableContainer>
+        
+            </Grid>
+            </Grid>
+            );
+        }
 }
